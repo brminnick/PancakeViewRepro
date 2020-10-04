3f64168aae9ddff40585a708d7fd813c69bcbfb2
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-10-04 12:00:25 -0700

    Initial commit

---
+++ App.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.PancakeView;
+
+namespace PancakeViewRepro
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new ContentPage
+            {
+                Content = new PancakeView()
+            };
+        }
+    }
+}
